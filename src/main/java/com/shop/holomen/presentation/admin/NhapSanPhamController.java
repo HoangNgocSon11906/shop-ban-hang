c37add32b0513fcf9bdf92fb6f5ab537cd4a7c29
Hoang Ngoc Son <hoangngocson@asoview.vn>
2021-05-12 16:01:16 +0700

    edit

--- src/main/java/com/shop/holomen/presentation/admin/NhapSanPhamController.java
+++ src/main/java/com/shop/holomen/presentation/admin/NhapSanPhamController.java
@@ -58,7 +58,7 @@ public class NhapSanPhamController {
         return "admin/nhapSanPham";
     }
 
-    @RequestMapping(value = "/layGiaTri")
+    @RequestMapping(value = "/layGiaTri", params = "save")
     String layGiaTri(@ModelAttribute("sanPhamDetail") SanPhamDetail sanPhamDetail, @ModelAttribute("multiPartFile") FormUpLoadImages file,
                      @ModelAttribute("multiPartFile1") FormUpLoadImages1 file1,
                      @ModelAttribute("multiPartFile2") FormUpLoadImages2 file2,
@@ -106,5 +106,55 @@ public class NhapSanPhamController {
                     "You successfully uploaded '");
 
         return String.format("redirect:/hienthi");
+    }
+
+    @RequestMapping(value = "/layGiaTri", params = "saveAndContinue")
+    String tieptuc(@ModelAttribute("sanPhamDetail") SanPhamDetail sanPhamDetail, @ModelAttribute("multiPartFile") FormUpLoadImages file,
+                     @ModelAttribute("multiPartFile1") FormUpLoadImages1 file1,
+                     @ModelAttribute("multiPartFile2") FormUpLoadImages2 file2,
+                     @ModelAttribute("multiPartFile3") FormUpLoadImages3 file3,
+                     @ModelAttribute("multiPartFile4") FormUpLoadImages4 file4,
+                     RedirectAttributes redirectAttributes) {
+
+        try {
+
+            // Get the file and save it somewhere
+            byte[] bytes = file.getMultipartFiles().getBytes();
+            Path path = Paths.get(UPLOADED_FOLDER + file.getMultipartFiles().getOriginalFilename());
+            byte[] bytes1 = file1.getAnh1().getBytes();
+            Path path1 = Paths.get(UPLOADED_FOLDER + file1.getAnh1().getOriginalFilename());
+            byte[] bytes2 = file2.getAnh2().getBytes();
+            Path path2 = Paths.get(UPLOADED_FOLDER + file2.getAnh2().getOriginalFilename());
+            byte[] bytes3 = file3.getAnh3().getBytes();
+            Path path3 = Paths.get(UPLOADED_FOLDER + file3.getAnh3().getOriginalFilename());
+            byte[] bytes4 = file4.getAnh4().getBytes();
+            Path path4 = Paths.get(UPLOADED_FOLDER + file4.getAnh4().getOriginalFilename());
+            Files.write(path, bytes);
+            Files.write(path1, bytes1);
+            Files.write(path2, bytes2);
+            Files.write(path3, bytes3);
+            Files.write(path4, bytes4);
+            redirectAttributes.addFlashAttribute("message",
+                    "You successfully uploaded '" + file.getMultipartFiles().getOriginalFilename() + "'");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String path = Paths.get(UPLOADED_FOLDER + file.getMultipartFiles().getOriginalFilename()).toString();
+        String path1 = Paths.get(UPLOADED_FOLDER + file1.getAnh1().getOriginalFilename()).toString();
+        String path2 = Paths.get(UPLOADED_FOLDER + file2.getAnh2().getOriginalFilename()).toString();
+        String path3 = Paths.get(UPLOADED_FOLDER + file3.getAnh3().getOriginalFilename()).toString();
+        String path4 = Paths.get(UPLOADED_FOLDER + file4.getAnh4().getOriginalFilename()).toString();
+        sanPhamDetail.setMainImage(new MainImage(new LinkHinh(path)));
+        sanPhamDetail.setImage1(new Image1(new LinkHinh(path1)));
+        sanPhamDetail.setImage2(new Image2(new LinkHinh(path2)));
+        sanPhamDetail.setImage3(new Image3(new LinkHinh(path3)));
+        sanPhamDetail.setImage4(new Image4(new LinkHinh(path4)));
+
+        nhapSanPhamService.insertSanPham(sanPhamDetail);
+
+        redirectAttributes.addFlashAttribute("message",
+                    "You successfully uploaded '");
+
+        return String.format("redirect:/nhap");
     }
 }
